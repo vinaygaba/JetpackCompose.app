512d1dbad6a1e5dd6eef155e24955d994b27d685
vinaygaba <vinaygaba@gmail.com>
2020-10-11 09:34:19 -0700

    Nit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,46 +17,9 @@ export default function JetpackComposeApp() {
           <FAQPageComponent listOfQnA={mockQnA} path="/faq" />
         </Router>
       </div>
-
-      {/* <Switch> */}
-      {/* <Route
-            path="/What-is-the-equivalent-of-:androidParam-in-Jetpack-Compose"
-            render={(props) => <IfThisThenThatComponent />}
-          /> */}
-      {/* <QuickBitesComponent path="/QuickBites" />
-        <FAQPageComponent listOfQnA={mockQnA} path="/FAQ" />
-        <IfThisThenThatComponent path="/What-is-the-equivalent-of-:androidParam-in-Jetpack-Compose" /> */}
-      {/* <Route path="/quick-bites">
-            <QuickBitesComponent />
-          </Route> */}
-      {/* <Route path="/wip">
-            <ComponentPreviewCardsSection
-              sectionTitle="title"
-              metadataArray={mockComponentPreviewCardMetadataArray}
-            />
-          </Route> */}
-      {/* <Route
-            path="/:androidParam"
-            render={(props) => <IfThisThenThatComponent />}
-          /> */}
-      {/* <Route path="/">
-            <IfThisThenThatComponent />
-          </Route> */}
-      {/* </Switch> */}
-      {/* </Router> */}
     </>
   );
 }
-
-// const RootApp = () => (
-//   <div className="SectionThree">
-//     <JetpackComposeApp />
-//   </div>
-// );
-
-// export default RootApp;
-
-// ReactDOM.render(<RootApp />, document.getElementById("root"));
 
 const useStyles = makeStyles({
   root: {
